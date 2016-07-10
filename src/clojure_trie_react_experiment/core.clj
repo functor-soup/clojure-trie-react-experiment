24a23d8aa217f3a006c9c0c4ea51ad35d8624957
luke <nerorevenge@gmail.com>
2016-07-09 21:02:00 -0400

    first commit - ring boilerplate

---
+++ src/clojure_trie_react_experiment/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-trie-react-experiment.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clojure_trie_react_experiment/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-trie-react-experiment.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-trie-react-experiment.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
