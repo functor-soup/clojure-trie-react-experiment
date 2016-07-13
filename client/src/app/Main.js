d9c547ca299f3658957bfacf093a5abe8c15491c
luke <nerorevenge@gmail.com>
2016-07-13 18:17:10 -0400

    a working client  - es6 + react +axios + material-ui

---
+++ client/src/app/Main.js
@@ -0,0 +1,62 @@
+/**
+ * In this file, we create a React component
+ * which incorporates components provided by Material-UI.
+ */
+import React, {Component} from 'react';
+import get from 'axios';
+import AutoComplete from 'material-ui/AutoComplete';
+import {deepOrange500} from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    paddingTop: 200,
+  },
+};
+
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500,
+  },
+});
+
+class Main extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      dataSource: [],
+    };
+    this.handleUpdate = this.handleUpdate.bind(this)
+  }
+
+  handleUpdate(input) { 
+    var self = this;
+    get(`http://localhost:9000/${input}`)
+     .then(function(value){
+        self.setState({
+            dataSource:value.data 
+           });
+       })
+    }
+
+
+  render() {
+
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div style={styles.container}>
+          <AutoComplete
+	  hintText="Type anything"
+	  dataSource={this.state.dataSource}
+	  onUpdateInput={this.handleUpdate}
+          />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default Main;
