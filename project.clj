bf1f1c07fb9f12a4b1d4f9723860a214ff1ff743
luke <nerorevenge@gmail.com>
2016-08-27 22:53:05 -0400

    addeded depenedency to take care deprecated handler module of compjure

--- project.clj
+++ project.clj
@@ -7,9 +7,11 @@
                  [compojure "1.5.1"]
                  [ring/ring-core "1.4.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
-                 [ring/ring-json "0.4.0"]]
+                 [ring/ring-json "0.4.0"]
+                 [ring/ring-defaults "0.2.1"]]
+
 
   :plugins [[lein-ring "0.9.7"]]
 
   :ring {:init clojure-trie-react-experiment.core/init
-         :handler clojure-trie-react-experiment.core/app})
+         :handler clojure-trie-react-experiment.core/site})
